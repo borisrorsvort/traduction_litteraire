4e63c6f5cad5069ec7630fc1f3d4c3edbd47e1e6
Boris Rorsvort <boris.rorsvort@gmail.com>
2015-09-22 08:58:13 +0200

    first commit

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-TraductionLitteraire::Application.config.secret_token = '53337ec7cfa0511fa03bf5e2d9acab254179e839b75061c43d01111750f2c8ab23d00a1b163e354fc9764b5ba4e8ccb08e1f873a8e9f9d8b03898d3bd4810490'
--- config/routes.rb
+++
@@ -1,3 +0,0 @@
-TraductionLitteraire::Application.routes.draw do
-  root :to => 'high_voltage/pages#show', :id => 'home'
-end

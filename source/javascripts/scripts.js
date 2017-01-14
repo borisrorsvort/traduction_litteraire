045993df6f4b655e70783ae153a78f8c173b1936
Boris Rorsvort <boris.rorsvort@gmail.com>
2017-01-14 21:51:08 +0100

    Cleanup

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -16,13 +16,6 @@ var cetlEvents = (function () {
       var endDate = new Date($(this).data('end-date'))
       var today = new Date()
 
-      console.log('startDate', startDate);
-
-      console.log('endDate', startDate >= today);
-
-      console.log('today', endDate >= today);
-
-
       if ((startDate >= today) || (endDate >= today)) {
         eventList.push({
           'start_date': startDate,
